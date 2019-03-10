83590e4ccbc3354daea0deb2d8e69b21c41337c7
rojefferson <rojefferson3@gmail.com>
2019-03-10 20:07:38 -0300

    configuracao inical do desafio

---
+++ codenation/python-1/desafio.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import  numpy as np
+dados = pd.read_csv("data.csv")
+print (dados.head())
---
+++ codenation/python-1/main.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+# Todas as perguntas são referentes ao arquivo `data.csv`
+# Você ** não ** pode utilizar o pandas e nem o numpy para este desafio.
+
+# **Q1.** Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo?
+# 
+def q_1():
+    pass
+
+# **Q2.** Quantos clubes (coluna `club`) diferentes existem no arquivo?
+def q_2():
+    pass
+
+# **Q3.** Liste o nome completo dos 20 primeiros jogadores de acordo com a coluna `full_name`.
+def q_3():
+    pass
+
+# **Q4.** Quem são os top 10 jogadores que ganham mais dinheiro (utilize as colunas `full_name` e `eur_wage`)?
+def q_4():
+    pass
+
+# **Q5.** Quem são os 10 jogadores mais velhos?
+def q_5():
+    pass
+
+# **Q6.** Conte quantos jogadores existem por idade. Para isso, construa um dicionário onde as chaves são as idades e os valores a contagem.
+def q_6():
+    pass
